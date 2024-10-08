815ff9adcde86dc23652049aa5e3d25711c21d41
Pratchavee Amornsetthachai <pratchavee.amorn@gmail.com>
2024-10-09 04:21:01 +0900

    cardgame

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Pratchavee Amornsetthachai on 2023/10/23.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
